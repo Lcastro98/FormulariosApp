15321cec7e2e01cdb897b3eb95ec891ed0705800
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-31 11:29:28 -0500

    FormArray

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ], Validators.required)
+  });
 
-  ngOnInit(): void {
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  campoNoValido( campo: string ) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    if(this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
   }
 
 }
